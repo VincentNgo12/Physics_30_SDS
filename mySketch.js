ef63317ab05262b8e93ce0b86c188af97f5e7dbb
VincentNgo12 <121452382+VincentNgo12@users.noreply.github.com>
2023-11-26 11:35:05 -0600

    Score System

--- mySketch.js
+++ mySketch.js
@@ -11,9 +11,12 @@ let cart;
 
 let g = 9.81; //Gravity
 let friction = 0.15; //Friction
-let Force = 500; //External Force
+let Force = 5000; //External Force
 const dt = 1e-3; // Time step
 const frames = 100;
+
+let score = 0;
+let high_score = 0;
 
 
 function setup() {
@@ -37,6 +40,8 @@ function draw() {
 	
 	for(let i=0; i<frames; i++){
 		background(70);
+		
+		show_score(); //Display Player Score
 
 		fill(255); // Color of the platform
 		rect(platformX, platformY, platformWidth, platformHeight);
@@ -49,7 +54,7 @@ function draw() {
 		if (keyIsDown(RIGHT_ARROW)) {
 			cart.force = createVector(Force,0);
 		}
-		
+
 		
 		cart.update(); //Update and Show Cart
 		cart.show();
@@ -58,6 +63,20 @@ function draw() {
 	}
 }
 
+
+function show_score(){
+	push();
+	// Display Score
+  fill(255);
+	scale(1, -1); //Rescale
+  textSize(36);
+  text(`Score: ${score.toFixed(1)}`, width / 2-220, -height / 2+80);
+
+  // Display High Score
+  textSize(24);
+  text(`High Score: ${high_score.toFixed(1)}`, width / 2-220, -height / 2+130);
+	pop();
+}
 
 function setCartesianCoordinate(){
 	// Translate to the middle of the canvas
